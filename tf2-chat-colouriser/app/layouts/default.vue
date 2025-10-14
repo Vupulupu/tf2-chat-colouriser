2f1f0ca29738bb58a83821b324e15e4e8b1ecd53
Vupulupu <71806946+Vupulupu@users.noreply.github.com>
2025-10-14 16:33:29 -0600

    Add app footer
    Fixed footer at the bottom of the page, containing version and credits
    (me)

--- tf2-chat-colouriser/app/layouts/default.vue
+++ tf2-chat-colouriser/app/layouts/default.vue
@@ -6,10 +6,26 @@
 	<main>
 		<slot/>
 	</main>
+	<footer>
+		<p>v.2025.0a</p>
+		<p>Created by <a href="https://steamcommunity.com/id/Vupulupu/">Vupulupu</a></p>
+	</footer>
 </template>
 
 <style>
 	main {
 		margin-top: 100px;
+	}
+
+	footer {
+		box-sizing: border-box;
+		width: 100%;
+		padding: 10px;
+		display: flex;
+		justify-content: space-between;
+		position: fixed;
+		bottom: 0;
+		font-family: "jetbrains mono", "sans-serif";
+		font-size: 12pt;
 	}
 </style>
