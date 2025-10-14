3043124e4e1fe13600014fbaa739c6fc55204961
Vupulupu <71806946+Vupulupu@users.noreply.github.com>
2025-10-14 16:29:17 -0600

    Font imports
    Added all fonts currently needed. Manually defined a couple CSS
    font-faces to resolve small issues with auto additions

--- tf2-chat-colouriser/app/app.vue
+++ tf2-chat-colouriser/app/app.vue
@@ -8,6 +8,26 @@
 </template>
 
 <style>
+	@font-face {
+		font-family: "tf2 build";
+		src: local("tf2 build"),
+		     url("/fonts/tf/tf2build.ttf") format("truetype");
+		ascent-override: 100%; /* reset font ascent; regularly importing has a lower boundary for some reason */
+	}
+
+	@font-face {
+		font-family: "tf2 professor";
+		src: local("tf2 professor scaled"),
+		url("/fonts/tf/tf2professorscaled.ttf") format("truetype");
+	}
+
+	:root {
+		--verdana-font-size: 16pt;
+		--main-font-size: 24pt;
+		font-family: "verdana", "sans-serif";
+		font-size: var(--main-font-size);
+	}
+
 	body {
 		height: 100dvh;
 		display: flow-root;
