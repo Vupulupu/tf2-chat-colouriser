aca76f2ada8361db20e20a502493bd6ff755b04e
Vupulupu <71806946+Vupulupu@users.noreply.github.com>
2025-10-23 18:46:19 -0600

    Refactor: Move body styling back to app component
    Put the `body` styling back in the app component file, since it only
    applies to the `body` created in that file and does not need to be in
    the global.css file.

--- tf2-chat-colouriser/app/app.vue
+++ tf2-chat-colouriser/app/app.vue
@@ -1,5 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
 	<NuxtLayout>
 		<NuxtPage />
 	</NuxtLayout>
 </template>
+
+<style>
+	body {
+		height: 100dvh;
+		display: flow-root;
+		margin: 0;
+		background-color: var(--tf2-shadow-colour);
+		background-image: linear-gradient(to top, var(--tf2-shadow-colour), transparent 30%),
+		radial-gradient(transparent, hsl(14 42% 13%) 200%),
+		url("/mannworks-bk.png");
+		background-position: top center;
+		background-size: cover;
+		background-repeat: no-repeat;
+	}
+</style>
