2a8a879a1902a70bd24a3665a4fbb429bcfaa6f1
Vupulupu <71806946+Vupulupu@users.noreply.github.com>
2025-10-23 18:19:45 -0600

    Refactor: Separate global css fonts and colour scheme
    Move font declarations and styles to a new file, including font-related
    media queries. Colour scheme file includes variable declarations for the
    global colour scheme. Global file still handles colour application.

--- tf2-chat-colouriser/nuxt.config.ts
+++ tf2-chat-colouriser/nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   ssr: false,
+  css: ["/assets/css/global.css"],
   vite: {
     build: {
       rollupOptions: {
