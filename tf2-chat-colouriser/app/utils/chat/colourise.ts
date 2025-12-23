e3e8e1429c63b97eb294f8ddb78b17446b2dfe1a
Vupulupu <71806946+Vupulupu@users.noreply.github.com>
2025-12-23 08:53:44 -0700

    Add function to update colouredRanges on input changes
    This new function adjusts the indices of the coloured ranges based on
    new changes made to the input— both additions and removals, thanks to
    the string difference util created a few commits ago. In essence, it is
    supposed to extend and contract coloured sections when characters are
    added or removed, respectively.

--- tf2-chat-colouriser/app/utils/chat/colourise.ts
+++ tf2-chat-colouriser/app/utils/chat/colourise.ts
@@ -1,4 +1,5 @@
 import { ColouredRange } from "~/utils/chat/coloured-range";
+import { IndexRange } from "~/utils/chat/index-range";
 
 /*
 if a colourised range starts inside new range, move start to end of new range.
@@ -21,4 +22,45 @@ export function applyColour(newSubstr: ColouredRange, colourisedRanges: Ref<Colo
 
 	colourisedRanges.value.push(newSubstr);
 	colourisedRanges.value.sort((a: ColouredRange, b: ColouredRange) => { return a.compare(b) });
+}
+
+/*
+if selection is before range, offset range indices by change.
+if selection subsumes range (subset of indices), delete range.
+if range includes selection (proper subset, starts & ends !=), move end of range to end+change.
+if start in but end not in range (including range's end), move end of range to start+max(change,0).
+if start not but end in range, move start of range to end+change.
+*/
+export function updateColour(strChange: number, colouredRanges: Ref<ColouredRange[]>, selection: IndexRange ): void {
+	selection.startIndex = Math.max(selection.startIndex, 0);
+	if (selection.endIndex < selection.startIndex) selection.endIndex = selection.startIndex;
+	for (let i:number=0; i<colouredRanges.value.length; i++) {
+		const currRange: ColouredRange | undefined = colouredRanges.value[i];
+		if (currRange) {
+			if (selection.endIndex <= currRange.startIndex) {
+				// selection is entirely before range
+				currRange.startIndex += strChange;
+				currRange.endIndex += strChange;
+			} else if (selection.subsumes(currRange)) {
+				// selection entirely surrounds range (or is exactly equal)
+				colouredRanges.value = colouredRanges.value.toSpliced(i--, 1)
+			} else if (currRange.includes(selection)) {
+				// selection is entirely within range (both start and end)
+				currRange.endIndex += strChange;
+			} else if (currRange.contains(selection.startIndex)) {
+				// only selection's start is within range (because previous branch implies end isn't
+				currRange.endIndex = selection.startIndex + Math.max(selection.endIndex-selection.startIndex + strChange, 0);
+			} else if (currRange.contains(selection.endIndex)) {
+				// only selection's end is within range (because previous branches imply start isn't
+				currRange.startIndex = selection.endIndex + strChange;
+			} else if (selection.endIndex===currRange.endIndex) {
+				// special branch when selection (start & end) is directly after range
+				// start index is implied to equal the end because it is not before or within the range
+				currRange.endIndex = selection.startIndex + strChange;
+			} else if (selection.startIndex===currRange.endIndex) {
+				// special special branch when only selection's start is directly after range
+				currRange.endIndex += selection.endIndex-selection.startIndex + strChange;
+			}
+		}
+	}
 }
